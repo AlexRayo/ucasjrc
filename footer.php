34ad9f2402ad8dc702f266a05f1422e295c46f01
Heisenberg <alexrayo.net@gmail.com>
2024-01-18 16:21:34 -0600

    add footer widgets, thumbnail img to single posts

--- footer.php
+++ footer.php
@@ -1,17 +1,26 @@
-
 </main>
 
-<?php do_action( 'tailpress_content_end' ); ?>
+<?php do_action('tailpress_content_end'); ?>
 
 </div>
 
-<?php do_action( 'tailpress_content_after' ); ?>
+<?php do_action('tailpress_content_after'); ?>
 
 <footer id="colophon" class="site-footer bg-gray-50 py-12" role="contentinfo">
-	<?php do_action( 'tailpress_footer' ); ?>
+	<div class="container mx-auto flex-col flex md:flex-row justify-between gap-8">
+		<?php
+		if (is_active_sidebar('footer-1')) {
+			dynamic_sidebar('footer-1');
+		}
+		?>
+
+	</div>
+	<?php do_action('tailpress_footer'); ?>
 
 	<div class="container mx-auto text-center text-gray-500">
-		&copy; <?php echo date_i18n( 'Y' );?> - <?php echo get_bloginfo( 'name' );?>
+		&copy;
+		<?php echo date_i18n('Y'); ?> -
+		<?php echo get_bloginfo('name'); ?>
 	</div>
 </footer>
 
@@ -20,4 +29,5 @@
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,24 +1,32 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header mb-4">
-		<?php the_title( sprintf( '<h1 class="entry-title text-2xl lg:text-5xl font-extrabold leading-tight mb-1"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-		<time datetime="<?php echo get_the_date( 'c' ); ?>" itemprop="datePublished" class="text-sm text-gray-700"><?php echo get_the_date(); ?></time>
+		<?php if (has_post_thumbnail()): ?>
+			<div class="h-72 w-full overflow-hidden rounded-md">
+				<?php the_post_thumbnail('large', array('class' => 'w-full h-auto')); ?>
+			</div>
+		<?php endif; ?>
+
+		<?php the_title(sprintf('<h1 class="mt-4 md:mt-8 entry-title text-2xl lg:text-5xl font-extrabold leading-tight mb-1"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h1>'); ?>
+		<time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished" class="text-sm text-gray-700">
+			<?php echo get_the_date(); ?>
+		</time>
 	</header>
 
 	<div class="entry-content">
 		<?php the_content(); ?>
 
 		<?php
-			wp_link_pages(
-				array(
-					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'tailpress' ) . '</span>',
-					'after'       => '</div>',
-					'link_before' => '<span>',
-					'link_after'  => '</span>',
-					'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'tailpress' ) . ' </span>%',
-					'separator'   => '<span class="screen-reader-text">, </span>',
-				)
-			);
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links"><span class="page-links-title">' . __('Pages:', 'tailpress') . '</span>',
+				'after' => '</div>',
+				'link_before' => '<span>',
+				'link_after' => '</span>',
+				'pagelink' => '<span class="screen-reader-text">' . __('Page', 'tailpress') . ' </span>%',
+				'separator' => '<span class="screen-reader-text">, </span>',
+			)
+		);
 		?>
 	</div>
 
